728afa7390f1d5614879bad7122c616ea0b5f7ad
infog2 dev <roney@infog2.com.br>
2016-05-03 14:56:27 -0300

    Primeira versão chave

---
+++ chave/FrmChave.dpr
@@ -0,0 +1,14 @@
+program FrmChave;
+
+uses
+  Forms,
+  uFrmChave in 'uFrmChave.pas' {Form1},
+  wcrypt2 in '..\Fontes\Wcrypt2.pas';
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
